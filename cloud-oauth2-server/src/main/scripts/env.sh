bc273436bd51c44708ce928161d35610babbb6dd
melin <libinsong1204@gmail.com>
2014-09-22 11:35:30 +0800

    修改脚本

--- cloud-oauth2-server/src/main/scripts/env.sh
+++ cloud-oauth2-server/src/main/scripts/env.sh
@@ -20,7 +20,27 @@ SERVER_NAME="OauthServer"
 export JMX_PORT=4001
 export CLASSPATH=$BASE_DIR/conf:$(ls $BASE_DIR/lib/*.jar | tr '\n' :)
 
-#UEAP jvm args
-BASE_APP_ARGS=""
-BASE_JVM_ARGS="-Xmx512m -Xms256m -server"
-APP_JVM_ARGS="$BASE_JVM_ARGS -cp $CLASSPATH"
+#JVM args
+if test -z "$SUPERDIAMOND_PROFILE"; then
+    export SUPERDIAMOND_PROFILE=development
+fi
+
+#添加logback.xml文件中profile值
+sed -ibak -e "s/logback-.*.xml/logback-$SUPERDIAMOND_PROFILE.xml/g" $BASE_DIR/conf/logback.xml
+
+BASE_APP_ARGS="-Dsuperdiamond.host=172.16.79.21 -Dsuperdiamond.port=8283 -Dsuperdiamond.projcode=cloud-service"
+
+BASE_JVM_ARGS_DEVELOPMENT="-Xmx512m -Xms256m -server"
+BASE_JVM_ARGS_TEST="-Xmx512m -Xms256m -server"
+BASE_JVM_ARGS_BUILD="-Xmx512m -Xms256m -server"
+BASE_JVM_ARGS_PRODUCTION="-Xmx512m -Xms256m -server"
+
+if [ $SUPERDIAMOND_PROFILE = "test" ];then
+    APP_JVM_ARGS="$BASE_JVM_ARGS_TEST -cp $CLASSPATH"
+elif [ $SUPERDIAMOND_PROFILE = "build" ];then
+    APP_JVM_ARGS="$BASE_JVM_ARGS_BUILD -cp $CLASSPATH"
+elif [ $SUPERDIAMOND_PROFILE = "production" ];then
+    APP_JVM_ARGS="$BASE_JVM_ARGS_PRODUCTION -cp $CLASSPATH"
+else
+    APP_JVM_ARGS="$BASE_JVM_ARGS_DEVELOPMENT -cp $CLASSPATH"
+fi
